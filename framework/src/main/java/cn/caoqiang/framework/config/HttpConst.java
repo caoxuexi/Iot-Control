f0ea658498739e22f4701d5e4b4ba95489126d96
caoxuexi <969718359@qq.com>
2021-07-03 10:59:11 +0800

    增加http传输的支持

--- framework/src/main/java/cn/caoqiang/framework/config/HttpConst.java
+++ framework/src/main/java/cn/caoqiang/framework/config/HttpConst.java
@@ -2,7 +2,7 @@ package cn.caoqiang.framework.config;
 
 public class HttpConst {
     //    发起请求的接口
-    public static  final String IOT_SITE="http://10.128.213.200:80";
+    public static  final String IOT_SITE="http://10.128.221.200:80";
     public static final String IOT_LED="/led";
     public static final String IOT_DHT="/search?keywords=dht";
     public static final String RED_ON="led1=on";
